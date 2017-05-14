a10089a779774e07f9087e1e32f119c7fb1ee0a9
vikasvking <vikas.vk711@gmail.com>
2017-05-15 02:38:53 +0530

    initial commit

---
+++ app/src/main/java/com/android/haplohealthclinic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.haplohealthclinic;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int TIME=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,GettingStarted.class));
+                finish();
+            }
+        },TIME);
+    }
+}
